ca790c88945699d21a7fdc7da0bdf1c5b06cb7a4
YEONOC <chlqhtjr752@gmail.com>
2023-12-19 22:15:26 +0900

    GameInstance / GameInstance-Character

---
+++ Source/R1/System/R1GameInstance.cpp
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "System/R1GameInstance.h"
+
+UR1GameInstance::UR1GameInstance(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+
+}
+
+void UR1GameInstance::Init()
+{
+	Super(Init);
+}
+
+void UR1GameInstance::Shutdown()
+{
+	Super(Shutdown);
+}
